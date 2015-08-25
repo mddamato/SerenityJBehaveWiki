c066338475bafe56bd8dac8bb2126ad92417b41e
mongoos2006 <michael.d.damato@gmail.com>
2015-08-25 18:19:36 -0400

    fixed some things

--- src/test/java/example/pages/WikiMainPage.java
+++ src/test/java/example/pages/WikiMainPage.java
@@ -7,14 +7,13 @@ package example.pages;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
-
 import org.openqa.selenium.By;
 
 public class WikiMainPage extends PageObject{
 
 //	@FindBy(id = "searchInput")
 //	protected WebElementFacade searchBox;
-	
+
 	public void searchFor(String searchString)
 	{
 		getDriver().findElement(By.id("searchInput")).sendKeys(searchString);
@@ -22,7 +21,7 @@ public class WikiMainPage extends PageObject{
 		//searchBox.typeAndEnter(searchString);
 	}
 
-	public void clickOnSideMenuOption(String sideMenuSelection) 
+	public void clickOnSideMenuOption(String sideMenuSelection)
 	{
 		if(sideMenuSelection.equalsIgnoreCase("Main Page"))
 			getDriver().findElement(By.id("n-mainpage-description")).findElement(By.tagName("a")).click();
@@ -36,5 +35,5 @@ public class WikiMainPage extends PageObject{
 					if(sideMenuSelection.equalsIgnoreCase("Random Article"))
 						getDriver().findElement(By.id("n-randompage")).findElement(By.tagName("a")).click();
 	}
-	
+
 }
--- src/test/resources/stories/Searching.story
+++ src/test/resources/stories/Searching.story
@@ -1,9 +1,9 @@
 Searching for an article
 
 Narrative:
-when an article title matches search criteria given in the 
+when an article title matches search criteria given in the
 search box then that wikipedia article should open immediately
-					 
+
 Scenario:  open the Hello World Program article
 Given i am on the wikipedia home page
 When i search for the article '"Hello, World!" program'
@@ -14,8 +14,7 @@ Given i am on the wikipedia home page
 When i search for the article 'Foobar'
 Then the 'Foobar' article should be displayed
 
-Scenario:  open the Foobar article
+Scenario:  open the Behavior-driven development article
 Given i am on the wikipedia home page
-When i search for the article 'Selenium (software)'
-Then the 'Selenium (software)' article should be displayed
-
+When i search for the article 'Behavior-driven development'
+Then the 'Behavior-driven development' article should be displayed
